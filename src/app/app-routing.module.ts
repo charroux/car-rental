0ab72564feeb696c08715c0da85d2764f730371a
orchaland <orchaland@gmail.com>
2019-10-15 13:51:41 +0200

    car detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CarsComponent } from './cars/cars.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/cars',
+    pathMatch: 'full'
+  },
+  { path: 'car',
+    component: CarDetailComponent
+  },
+  { path: 'cars',
+    component: CarsComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarsComponent } from './cars/cars.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
