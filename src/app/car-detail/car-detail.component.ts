36a3b23339f80d797fd14ba1d142344e6be8d569
orchaland <orchaland@gmail.com>
2019-10-15 13:51:58 +0200

    car detail

---
+++ src/app/car-detail/car-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-car-detail',
+  templateUrl: './car-detail.component.html',
+  styleUrls: ['./car-detail.component.css']
+})
+export class CarDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/cars.ts
@@ -0,0 +1,18 @@
+
+export const cars = [
+  {
+    brand: 'Ferrari',
+    price: 799,
+    plateNumber: 'AA11BB'
+  },
+  {
+    name: 'Porshe',
+    price: 699,
+    plateNumber: 'BB22CC'
+  },
+  {
+    name: 'Maserati',
+    price: 299,
+    plateNumber: 'CC33DD'
+  }
+];
